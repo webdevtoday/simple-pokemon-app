bd89c3ecaad77766d9059ebb7c0b0856a9bb1b09
webdevtoday <coderwd@gmail.com>
2022-11-13 01:47:58 +0200

    Add theme switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { useState } from "react";
-import { styled, alpha } from "@mui/material/styles";
+import { useState, createContext, useMemo } from "react";
+import {
+  styled,
+  alpha,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material/styles";
 import {
   Box,
   AppBar,
@@ -18,11 +23,15 @@ import {
   TablePagination,
   Stack,
   Unstable_Grid2 as Grid,
+  Switch,
+  FormControlLabel,
 } from "@mui/material";
 import {
   Search as SearchIcon,
   KeyboardArrowUp as KeyboardArrowUpIcon,
 } from "@mui/icons-material";
+
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -60,6 +69,52 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
         width: "20ch",
       },
     },
+  },
+}));
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    width: 32,
+    height: 32,
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    borderRadius: 20 / 2,
   },
 }));
 
@@ -105,8 +160,20 @@ const ScrollTop = ({ children }) => {
 };
 
 const App = () => {
+  const [mode, setMode] = useState("light");
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(() => createTheme({ palette: { mode } }), [mode]);
 
   const handleChangePage = (event, value) => {
     setPage(value);
@@ -118,78 +185,98 @@ const App = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <HideOnScroll>
-        <AppBar>
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ flexGrow: 1 }}
-            >
-              My Pokédex
-            </Typography>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search..."
-                inputProps={{ "area-label": "search" }}
-              />
-            </Search>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar id="back-to-top-anchor" />
-      <Container>
-        <Box sx={{ my: 2 }}>
-          <Grid container spacing={2}>
-            {[...new Array(10)].map((v, i) => (
-              <Grid key={i} xs={12} sm={6} md={4} xl={3}>
-                <Card>
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
-                    alt="Bulbasaur"
-                  />
-                  <CardHeader
-                    title="Shrimp and Chorizo Paella"
-                    subheader="September 14, 2016"
-                  />
-                  <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                      This impressive paella is a perfect party dish and a fun
-                      meal to cook together with your guests. Add 1 cup of
-                      frozen peas along with the mussels, if you like.
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 1 }}>
+          <HideOnScroll>
+            <AppBar>
+              <Toolbar>
+                <Grid
+                  container
+                  sx={{
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    width: "100%",
+                  }}
+                >
+                  <Grid>
+                    <Typography variant="h6" noWrap component="div">
+                      My Pokédex
                     </Typography>
-                  </CardContent>
-                </Card>
+                  </Grid>
+                  <Grid>
+                    <FormControlLabel
+                      control={
+                        <MaterialUISwitch sx={{ m: 1 }} defaultChecked />
+                      }
+                      onChange={colorMode.toggleColorMode}
+                    />
+                  </Grid>
+                  <Grid>
+                    <Search>
+                      <SearchIconWrapper>
+                        <SearchIcon />
+                      </SearchIconWrapper>
+                      <StyledInputBase
+                        placeholder="Search..."
+                        inputProps={{ "area-label": "search" }}
+                      />
+                    </Search>
+                  </Grid>
+                </Grid>
+              </Toolbar>
+            </AppBar>
+          </HideOnScroll>
+          <Toolbar id="back-to-top-anchor" />
+          <Container>
+            <Box sx={{ my: 2 }}>
+              <Grid container spacing={2}>
+                {[...new Array(10)].map((v, i) => (
+                  <Grid key={i} xs={12} sm={6} md={4} xl={3}>
+                    <Card>
+                      <CardMedia
+                        component="img"
+                        height="194"
+                        image="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+                        alt="Bulbasaur"
+                      />
+                      <CardHeader
+                        title="Shrimp and Chorizo Paella"
+                        subheader="September 14, 2016"
+                      />
+                      <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                          This impressive paella is a perfect party dish and a
+                          fun meal to cook together with your guests. Add 1 cup
+                          of frozen peas along with the mussels, if you like.
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
 
-          <Stack>
-            <Typography>Page: {page}</Typography>
-            <TablePagination
-              component="div"
-              count={100}
-              page={page}
-              onPageChange={handleChangePage}
-              rowsPerPage={rowsPerPage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-            />
-          </Stack>
+              <Stack>
+                <Typography>Page: {page}</Typography>
+                <TablePagination
+                  component="div"
+                  count={100}
+                  page={page}
+                  onPageChange={handleChangePage}
+                  rowsPerPage={rowsPerPage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </Stack>
+            </Box>
+          </Container>
+          <ScrollTop>
+            <Fab size="small" aria-label="scroll back to top">
+              <KeyboardArrowUpIcon />
+            </Fab>
+          </ScrollTop>
         </Box>
-      </Container>
-      <ScrollTop>
-        <Fab size="small" aria-label="scroll back to top">
-          <KeyboardArrowUpIcon />
-        </Fab>
-      </ScrollTop>
-    </Box>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
