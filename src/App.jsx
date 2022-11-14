84f0a78f8f41ae3ae4fd449383153543e11fba41
webdevtoday <coderwd@gmail.com>
2022-11-14 07:00:43 +0200

    Prototype design

--- src/App.jsx
+++ src/App.jsx
@@ -1,179 +1,63 @@
-import { useState, createContext, useMemo } from "react";
-import {
-  styled,
-  alpha,
-  createTheme,
-  ThemeProvider,
-} from "@mui/material/styles";
+import { useState, useEffect } from "react";
 import {
   Box,
   AppBar,
   Toolbar,
   Typography,
-  InputBase,
-  useScrollTrigger,
-  Slide,
   Container,
-  Fab,
-  Fade,
   Card,
   CardHeader,
   CardMedia,
   CardContent,
   TablePagination,
-  Stack,
   Unstable_Grid2 as Grid,
-  Switch,
-  FormControlLabel,
+  Autocomplete,
+  TextField,
+  CircularProgress,
 } from "@mui/material";
-import {
-  Search as SearchIcon,
-  KeyboardArrowUp as KeyboardArrowUpIcon,
-} from "@mui/icons-material";
+import parse from "autosuggest-highlight/parse";
+import match from "autosuggest-highlight/match";
 
-const ColorModeContext = createContext({ toggleColorMode: () => {} });
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
-const MaterialUISwitch = styled(Switch)(({ theme }) => ({
-  width: 62,
-  height: 34,
-  padding: 7,
-  "& .MuiSwitch-switchBase": {
-    margin: 1,
-    padding: 0,
-    transform: "translateX(6px)",
-    "&.Mui-checked": {
-      color: "#fff",
-      transform: "translateX(22px)",
-      "& .MuiSwitch-thumb:before": {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          "#fff"
-        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
-      },
-      "& + .MuiSwitch-track": {
-        opacity: 1,
-        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-      },
-    },
-  },
-  "& .MuiSwitch-thumb": {
-    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
-    width: 32,
-    height: 32,
-    "&:before": {
-      content: "''",
-      position: "absolute",
-      width: "100%",
-      height: "100%",
-      left: 0,
-      top: 0,
-      backgroundRepeat: "no-repeat",
-      backgroundPosition: "center",
-      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        "#fff"
-      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
-    },
-  },
-  "& .MuiSwitch-track": {
-    opacity: 1,
-    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
-    borderRadius: 20 / 2,
-  },
-}));
-
-const HideOnScroll = ({ children }) => {
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-};
-
-const ScrollTop = ({ children }) => {
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 100,
+const sleep = (delay = 0) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
   });
-
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector(
-      "#back-to-top-anchor"
-    );
-
-    if (anchor) {
-      anchor.scrollIntoView({
-        block: "center",
-      });
-    }
-  };
-
-  return (
-    <Fade in={trigger}>
-      <Box
-        onClick={handleClick}
-        role="presentation"
-        sx={{ position: "fixed", bottom: 16, right: 16 }}
-      >
-        {children}
-      </Box>
-    </Fade>
-  );
 };
 
 const App = () => {
-  const [mode, setMode] = useState("light");
   const [page, setPage] = useState(1);
   const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-      },
-    }),
-    []
-  );
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
 
-  const theme = useMemo(() => createTheme({ palette: { mode } }), [mode]);
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      await sleep(1e3); // For demo purposes.
+
+      if (active) {
+        // TODO: SET AUTOCOMPLETE OPTIONS
+        setOptions([]);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
 
   const handleChangePage = (event, value) => {
     setPage(value);
@@ -185,98 +69,177 @@ const App = () => {
   };
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <Box sx={{ flexGrow: 1 }}>
-          <HideOnScroll>
-            <AppBar>
-              <Toolbar>
-                <Grid
-                  container
-                  sx={{
-                    alignItems: "center",
-                    justifyContent: "space-between",
-                    width: "100%",
-                  }}
-                >
-                  <Grid>
-                    <Typography variant="h6" noWrap component="div">
-                      My Pokédex
-                    </Typography>
-                  </Grid>
-                  <Grid>
-                    <FormControlLabel
-                      control={
-                        <MaterialUISwitch sx={{ m: 1 }} defaultChecked />
-                      }
-                      onChange={colorMode.toggleColorMode}
-                    />
-                  </Grid>
-                  <Grid>
-                    <Search>
-                      <SearchIconWrapper>
-                        <SearchIcon />
-                      </SearchIconWrapper>
-                      <StyledInputBase
-                        placeholder="Search..."
-                        inputProps={{ "area-label": "search" }}
-                      />
-                    </Search>
-                  </Grid>
-                </Grid>
-              </Toolbar>
-            </AppBar>
-          </HideOnScroll>
-          <Toolbar id="back-to-top-anchor" />
-          <Container>
-            <Box sx={{ my: 2 }}>
-              <Grid container spacing={2}>
-                {[...new Array(10)].map((v, i) => (
-                  <Grid key={i} xs={12} sm={6} md={4} xl={3}>
-                    <Card>
-                      <CardMedia
-                        component="img"
-                        height="194"
-                        image="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
-                        alt="Bulbasaur"
-                      />
-                      <CardHeader
-                        title="Shrimp and Chorizo Paella"
-                        subheader="September 14, 2016"
-                      />
-                      <CardContent>
-                        <Typography variant="body2" color="text.secondary">
-                          This impressive paella is a perfect party dish and a
-                          fun meal to cook together with your guests. Add 1 cup
-                          of frozen peas along with the mussels, if you like.
-                        </Typography>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" id="back-to-top-anchor">
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            My Pokédex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Box sx={{ my: 2 }}>
+          <Grid container>
+            <Grid xs={12} md={6}>
+              <Autocomplete
+                options={[]}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Pokémon types"
+                    placeholder="Pokémon types..."
+                  />
+                )}
+                renderOption={(props, option, { inputValue }) => {
+                  const matches = match(option.label, inputValue, {
+                    insideWords: true,
+                  });
+                  const parts = parse(option.label, matches);
 
-              <Stack>
-                <Typography>Page: {page}</Typography>
-                <TablePagination
-                  component="div"
-                  count={100}
-                  page={page}
-                  onPageChange={handleChangePage}
-                  rowsPerPage={rowsPerPage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-              </Stack>
-            </Box>
-          </Container>
-          <ScrollTop>
-            <Fab size="small" aria-label="scroll back to top">
-              <KeyboardArrowUpIcon />
-            </Fab>
-          </ScrollTop>
+                  return (
+                    <li {...props}>
+                      <div>
+                        {parts.map((part, index) => (
+                          <span
+                            key={index}
+                            style={{ fontWeight: part.highlight ? 700 : 400 }}
+                          >
+                            {part.text}
+                          </span>
+                        ))}
+                      </div>
+                    </li>
+                  );
+                }}
+                multiple
+                limitTags={2}
+                getOptionLabel={(option) => option.label}
+                sx={{ maxWidth: 500 }}
+              />
+            </Grid>
+            <Grid
+              xs={12}
+              md={6}
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                mt: { xs: 2, md: 0 },
+              }}
+            >
+              <Autocomplete
+                freeSolo
+                sx={{ maxWidth: 300, width: "100%" }}
+                open={open}
+                onOpen={() => {
+                  setOpen(true);
+                }}
+                onClose={() => {
+                  setOpen(false);
+                }}
+                isOptionEqualToValue={(option, value) =>
+                  option.label === value.label
+                }
+                getOptionLabel={(option) => option.label}
+                options={options}
+                loading={loading}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Pokémon name"
+                    InputProps={{
+                      ...params.InputProps,
+                      endAdornment: (
+                        <>
+                          {loading ? (
+                            <CircularProgress color="inherit" size={20} />
+                          ) : null}
+                          {params.InputProps.endAdornment}
+                        </>
+                      ),
+                    }}
+                  />
+                )}
+                renderOption={(props, option, { inputValue }) => {
+                  const matches = match(option.label, inputValue, {
+                    insideWords: true,
+                  });
+                  const parts = parse(option.label, matches);
+
+                  return (
+                    <li {...props}>
+                      <div>
+                        {parts.map((part, index) => (
+                          <span
+                            key={index}
+                            style={{ fontWeight: part.highlight ? 700 : 400 }}
+                          >
+                            {part.text}
+                          </span>
+                        ))}
+                      </div>
+                    </li>
+                  );
+                }}
+              />
+            </Grid>
+          </Grid>
         </Box>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+      </Container>
+      <Container>
+        <Box sx={{ my: 2 }}>
+          <TablePagination
+            component="div"
+            count={100}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[10, 20, 50]}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            showFirstButton
+            showLastButton
+            labelRowsPerPage="Cards per page:"
+          />
+
+          <Grid container spacing={2}>
+            {[...new Array(10)].map((v, i) => (
+              <Grid key={i} xs={12} sm={6} md={4} xl={3}>
+                <Card>
+                  <CardMedia
+                    component="img"
+                    height="194"
+                    image="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+                    alt="Bulbasaur"
+                  />
+                  <CardHeader
+                    title="Pokemon Name"
+                    subheader="type (should visually look as a colored tag)"
+                  />
+                  <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                      the main pokemon stats (whichever additional pokemon info
+                      you want to show)
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+
+          <TablePagination
+            component="div"
+            count={100}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            rowsPerPageOptions={[10, 20, 50]}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            showFirstButton
+            showLastButton
+            labelRowsPerPage="Cards per page:"
+          />
+        </Box>
+      </Container>
+    </Box>
   );
 };
 
